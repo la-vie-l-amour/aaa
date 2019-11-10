09b731445bc79704487ef890f91df1211cd85c88
la-vie-l-amour <57085620+la-vie-l-amour@users.noreply.github.com>
2019-11-10 20:37:55 +0800

    a.h和a1.c文件包含和宏定义的综合运用
    算法很简单，用到了宏定义的嵌套

---
+++ a1.c
@@ -0,0 +1,14 @@
+#include"c:\users\86151\myinclude\a.h"
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	char s[]="CHINA";
+	a=1;b=2;c=3;d=4;
+	PR(D1,a);
+	PR(D2,a,b);
+	PR(D3,a,b,c);
+	PR(D4,a,b,c,d);
+	PR(S,s);
+	
+}
