93b3c55a6257552971f6d275eda8003585a31e80
la-vie-l-amour <57085620+la-vie-l-amour@users.noreply.github.com>
2019-11-17 10:36:15 +0800

    通过指针引用字符串
    将字符串b复制给字符串a，然后输出字符串a；27，28，29，30，那几行都会报错，不知道为什么，到时再改吧

---
+++ a9.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+void copy_string(char *from,char *to);
+int main()
+{
+	char *a="c lanuage";
+	char *b="math";
+	copy_string(b,a);
+	printf("%s",a);
+}
+/*void copy_string(char *from,char *to)
+{
+	int i;
+	for(i=0;from[i]!=0;i++)
+	   to[i]=from[i];
+	to[i]='\0';
+	    
+}*/
+/*void copy_string(char *from,char *to)
+{
+	while((*to=*from)!='\0')
+	{from++;
+	 to++;
+	}
+}*/
+/*void copy_string(char *from,char *to);
+{
+	//while((*to++=*from++)!='\0');
+	//while(*to++=*from++);
+	//for(;(*to++=*from++)!='\0';);
+	for(;*to++=*from++;);
+}*/
+/*void copy_string(char *from,char *to)
+{
+	while((*from)!='\0')
+	    *to++=*from++;
+	*to='\0';	
+}*/
+void copy_string(char from[],char to[])
+{
+	char *p1,*p2;
+	p1=from;
+	p2=to;	
+	while(*to++=*from++);
+}
