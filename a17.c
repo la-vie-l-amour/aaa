168de22b6dc734dbf990e8259b2ebcd61a1db129
la-vie-l-amour <57085620+la-vie-l-amour@users.noreply.github.com>
2019-12-01 19:02:27 +0800

    向文件写字符串
    从键盘读入若干个字符串，对他们按字母的大小顺序排列，然后把排好序的字符串送到磁盘文件中保存
    用数组对字符串进行的比大小

--- a17.c
+++ a17.c
@@ -1,30 +1,40 @@
 #include<stdio.h>
 #include<string.h>
-void cp(char *q1,char *q2);
+#include<stdlib.h>
 int main()
 {
-	char a,b,c;
-	char *p1,*p2,*p3;
-	p1=&a;
-	p2=&b;
-	p3=&c;
-	printf("please enter three strings\n");
-	scanf("%s\n%s\n%s\n",p1,p2,p3);
-	cp(p1,p2);
-	cp(p2,p3);
-	cp(p1,p2);
-	printf("%s\n%s\n%s\n",p1,p2,p3);
-	   
-}
-void cp(char *q1,char *q2)
-{
-	char *t;
-	char a[]="clll";
-	t=a;
-	if(strcmp(q1,q2)>0)
-	  {
-		t=q1;
-		q1=q2;
-		q2=t;
-	   }
+	FILE *fp;
+	char a[3][10],temp[10];
+	int i,j,k;
+	printf("please enter strings:\n");
+	for(i=0;i<3;i++)
+	   scanf("%s",a[i]);
+	for(i=0;i<2;i++)
+	{    k=i;
+	     for(j=i+1;j<3;j++)
+		if(strcmp(a[k],a[j])>0)
+	     		k=j;
+	     if(k!=i)
+	     {
+		strcpy(temp,a[i]);
+		strcpy(a[i],a[k]);
+		strcpy(a[k],temp);
+	      }
+	}
+
+	fp=fopen("a.dat","w");
+	if(fp==NULL)
+	{
+		printf("file open error");
+		exit(0);
+	}
+	
+	
+	printf("\n");
+	
+	for(i=0;i<3;i++)
+	{  fputs(a[i],fp);
+	   fputs("\n",fp);
+	  printf("%s\n",a[i]);
+	}
 }
