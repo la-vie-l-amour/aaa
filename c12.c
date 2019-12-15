25d2a536c6eb541dfc403cc792e5777d9b8bdc9f
la-vie-l-amour <57085620+la-vie-l-amour@users.noreply.github.com>
2019-12-15 11:37:37 +0800

    指向函数的指针的应用与c11同
    计算定积分（用用矩形法）

---
+++ c12.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float fun(float a,float b,float (*p)(float));
+	float f1(float x);
+	float f2(float x);
+	float f3(float x);
+	float s1,s2,s3,c,d;
+	
+	printf("please enter shangxian and xiaxian\n");
+	scanf("%f%f",&c,&d);
+	s1=fun(c,d,f1);
+	s2=fun(c,d,f2);
+	s3=fun(c,d,f3);	
+	printf("%f\n%f\n%f\n",s1,s2,s3);
+}
+float fun(float a,float b,float (*p)(float))
+{
+	float s=0,h,x=a;
+	int n=30,i;
+	h=(b-a)/n;
+	for(i=1;i<=n;i++)
+	{	x=x+h;
+		s=s+(*p)(x)*h;
+	}
+	return s;
+}
+float f1(float x)
+{
+	return(sin(x));
+}
+float f2(float x)
+{
+
+	return(cos(x));
+}
+float f3(float x)
+{
+	return(exp(x));
+} 
