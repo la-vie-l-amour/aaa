e265a02535c2e9c84e1473611bca58e58da3fd43
la-vie-l-amour <57085620+la-vie-l-amour@users.noreply.github.com>
2019-12-18 21:42:33 +0800

    这个答案并不完整，具体该法，我也不知道
    将一个5x5的矩形中最大的元素放在中心，4个角分别放4个最小的元素（顺序为从左到右，从上到下依次从小到大存放），写一个函数实现之。用main函数调用

--- d3.c
+++ d3.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	void fun(int (*p)[5],int n);
+	void fun(int *p);
 	int a[5][5];
 	int i,j;
 	for(i=0;i<5;i++)
 		for(j=0;j<5;j++)
 			scanf("%d",&a[i][j]);
-	fun(a,5);
+	fun(a[0]);
 	printf("\n");
 	for(i=0;i<5;i++)
 	{	for(j=0;j<5;j++)
@@ -15,44 +15,40 @@ int main()
 		printf("\n");
 	}
 }
-void fun(int (*p)[5],int n)
+void fun(int *p)
 {
 	int *pmin,*pmax,i,t;
-	pmin=*p;
-	pmax=*p;
-	for(i=0;i<5*n-1;i++)
-		if(*(*p+i)>*pmax)
-			pmax=*p+i;
+	pmin=p;
+	pmax=p;
+	for(i=0;i<25;i++)
+		if(*(p+i)>*pmax)
+			pmax=p+i;
 	
-	t=*(*p+12);
-	*(*p+12)=*pmax;
+	t=*(p+12);
+	*(p+12)=*pmax;
 	*pmax=t;
-	 for(i=0;i<24;i++)
-		if(*(*p+i)<*pmin)
-			pmin=*p+i;
-	 t=**p;
-	 **p=*pmin;
+	 for(i=0;i<25;i++)
+		if(*(p+i)<*pmin)
+			pmin=p+i;
+	 t=*p;
+	 *p=*pmin;
 	 *pmin=t; 
-	for(i=0;i<24;i++)
-		if((*(*p+i)<*pmin)&&(*(*p+i)!=**p))   
-			pmin=*p+i;    
-	 t=*(*p+4);
-	 *(*p+4)=*pmin;
+	for(i=0;i<25;i++)
+		if((*(p+i)<*pmin)&&(*(p+i)!=*p))   
+			pmin=p+i;    
+	 t=*(p+4);
+	 *(p+4)=*pmin;
 	 *pmin=t;  
-	for(i=0;i<24;i++)
-		if((*(*p+i)<*pmin)&&(*(*p+i)!=**p)&&(*(*p+i)!=*(*p+4)))   
-			pmin=*p+i;    
-	 t=*(*p+20);
-	 *(*p+20)=*pmin;
+	for(i=0;i<25;i++)
+		if((*(p+i)<*pmin)&&(*(p+i)!=*p)&&(*(p+i)!=*(p+4)))   
+			pmin=p+i;    
+	 t=*(p+20);
+	 *(p+20)=*pmin;
 	 *pmin=t; 
 	for(i=0;i<24;i++)
-		if((*(*p+i)<*pmin)&&(*(*p+i)!=**p)&&(*(*p+i)!=*(*p+4))&&(*(*p+i)!=*(*p+20)))   
-			pmin=*p+i;    
-	 t=*(*p+24);
-	 *(*p+24)=*pmin;
+		if((*(p+i)<*pmin)&&(*(p+i)!=*p)&&(*(p+i)!=*(p+4))&&(*(p+i)!=*(p+20)))   
+			pmin=p+i;    
+	 t=*(p+24);
+	 *(p+24)=*pmin;
 	 *pmin=t;                                                                                                                                                                           	  
 }
-
-	
-
-
